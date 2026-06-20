6cef0ad9a877bc90be39106ba8cf216fb79eeb08
Saravanakumar <135038409+saravanakumar1907@users.noreply.github.com>
2026-06-21 00:28:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ logging.basicConfig(
     format='%(asctime)s %(levelname)s %(message)s'
 )
 dynamodb = boto3.resource('dynamodb', region_name='ap-south-1')
-table = dynamodb.Table('Expense')
+table = dynamodb.Table('Expenses')
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
